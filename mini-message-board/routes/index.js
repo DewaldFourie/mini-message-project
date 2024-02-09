d8e385e229e37ef7254828f7c4b62d8b3c9a7bfe
Dewald <dewaldfourie0808@gmail.com>
2024-02-09 11:31:06 +0200

    added input form and basic logic

--- mini-message-board/routes/index.js
+++ mini-message-board/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Add a new Message',  })
+})
+
+router.post('/new', function(req, res, next) {
+  messageUser = req.body.name;
+  messageText = req.body.message;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date()
+  })
+
+  res.redirect('/')
+})
+
 module.exports = router;
+
